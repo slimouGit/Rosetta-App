20007b079deca7d39dac8b39716275ca21472418
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-03-01 17:28:10 +0100

    table

--- Sandbox/include/view_table.php
+++ Sandbox/include/view_table.php
@@ -4,11 +4,11 @@
     <thead>
         <tr>
             <th class="col-sm-1">Id</th>
-            <th class="col-sm-2 german">De</a></th>
-            <th class="col-sm-2 french">Fr</th>
-            <th class="col-sm-2 italy">It</th>
-            <th class="col-sm-2 english">En</th>
-            <th class="col-sm-2 ">Rubrik</th>
+            <th class="col-sm-2">De</a></th>
+            <th class="col-sm-2">Fr</th>
+            <th class="col-sm-2">It</th>
+            <th class="col-sm-2">En</th>
+            <th class="col-sm-2">Rubrik</th>
             <th class="col-sm-2">Info</th>
             <th class="col-sm-2">Carline</th>
             <th class="col-sm-2"></th>
@@ -25,6 +25,7 @@ while ($dsatz = mysqli_fetch_assoc($res))
 
 
     echo   "<tr>".
+
         "<td>" . $dsatz["id"]        . "</td>".
         "<td>" . $dsatz["de"]        . "</td>".
         "<td>" . $dsatz["fr"]        . "</td>".
