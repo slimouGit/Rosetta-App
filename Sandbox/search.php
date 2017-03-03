56fe34928086868bde2c953aea67c87f3ed5122d
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-03-03 13:00:44 +0100

    autocomplete

--- Sandbox/search.php
+++ Sandbox/search.php
@@ -1,9 +1,18 @@
 <?php
-//database configuration
+//offline
+
 $dbHost = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
 $dbName = 'rosetta-app';
+
+//online
+/*
+$dbHost = 'rdbms.strato.de';
+$dbUsername = 'U2881460';
+$dbPassword = 'Rosetta-App_1';
+$dbName = 'DB2881460';
+*/
 
 //connect with the database
 $db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
@@ -11,11 +20,29 @@ $db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 //get search term
 $searchTerm = $_GET['term'];
 
+//Pruefung, ob checkboxen ausgewaehlt wurden
+if(!empty($_POST['category'])) {
+    //das Array carline wird ueber implode in $car gespeichert
+    $cat = implode(',  ', $_POST['category']);
+}
+//falls keine checkbox aktiv, wird in allen Spalten gesucht
+if(empty($cat)) {
+    $cat = 'de, fr, it, en, rubrik, info, carline';
+}
+
+
 //get matched data from skills table
-$query = $db->query("SELECT * FROM rosetta_data WHERE CONCAT_WS('',de, fr) LIKE '%".$searchTerm."%' ORDER BY de ASC");
+$query = $db->query("SELECT * FROM rosetta_data WHERE CONCAT_WS('',$cat) LIKE '%".$searchTerm."%' ORDER BY de ASC");
 while ($row = $query->fetch_assoc()) {
     $data[] = $row['de'];
     $data[] = $row['fr'];
+    $data[] = $row['it'];
+    /*
+    $data[] = $row['en'];
+    $data[] = $row['rubrik'];
+    $data[] = $row['info'];
+    $data[] = $row['carline'];
+    */
 }
 
 //return json data
