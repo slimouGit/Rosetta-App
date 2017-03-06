5bacdcab6dd2d81e2acf3d90835d88eaa26a3548
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-03-06 15:04:15 +0100

    new utf8 entries

--- Sandbox/include/db_connect.php
+++ Sandbox/include/db_connect.php
@@ -4,6 +4,9 @@
 /*
 //online
 $con = mysqli_connect("rdbms.strato.de","U2881460","Rosetta-App_1");
+mysqli_query($con, "SET NAMES SET 'utf8'");
+mysqli_query($con, "SET character_set_client = 'utf8'");
+mysqli_query($con, "SET character_set_connection = 'utf8'");
 mysqli_select_db($con, "DB2881460");
 */
 
