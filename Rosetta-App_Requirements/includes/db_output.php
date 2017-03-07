76906525583776a9075bce8466eef92942531562
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-03-07 16:13:24 +0100

    editable requirements tool

--- Rosetta-App_Requirements/includes/db_output.php
+++ Rosetta-App_Requirements/includes/db_output.php
@@ -1,3 +1,51 @@
+<script type="text/javascript">
+    function send(ak,id)
+    {
+        if(ak==0)
+            document.f.ak.value = "in";
+        else if(ak==1)
+            document.f.ak.value = "up";
+        else if(ak==2)
+        {
+            if (confirm("Datensatz mit id " + id + " löschen?"))
+                document.f.ak.value = "de";
+            else
+                return;
+        }
+        document.f.id.value = id;
+        document.f.submit();
+    }
+</script>
+
+<?php
+
+/* Aktion ausf�hren */
+if(isset($_POST["ak"]))
+{
+    /* neu eintragen */
+    if($_POST["ak"]=="in")
+    {
+
+    }
+
+    /* �ndern */
+    else if($_POST["ak"]=="up")
+    {
+        echo "update";
+
+
+    }
+
+    /* l�schen */
+    else if($_POST["ak"]=="de")
+    {
+
+        echo "delete";
+    }
+}
+
+?>
+
 <?php
 
     //default-Wert fuer ID eines Eintrags
@@ -26,7 +74,7 @@
 
     //Datenausgabe
     while ($row = $result->fetch_assoc()) {
-                        
+        $id_nr = $row["id"];
         echo "<tr>";
                         
             echo "<td class=\"verbindlichkeit\" id=\"Verb_ID_$verbindlichkeit_ID\">" . $row["verbindlichkeit"] . "</td>";
@@ -35,6 +83,8 @@
             echo "<td class=\"author\">" . $row["author"] . "</td>";
             echo "<td class=\"titel\">" . $row["titel"] . "</td>";
             echo "<td class=\"beschreibung\">" . $row["beschreibung"] . "</td>";
+            echo "<td><a href='javascript:send(1,$id_nr);'><img src=\"img/button_agree.png\"></a></td>";
+            echo "<td><a href='javascript:send(2,$id_nr);'><img src=\"img/button_delete.png\"></a></td>";
             //echo "<td class=\"id\"><input type=\"checkbox\" id=\"Checkbox_$verbindlichkeit_ID\"></td>";
                         
         echo "</tr>";
