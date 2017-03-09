90bb40c50654d0ac2a0072c9eac3ccb5a02bf3c8
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-03-09 10:18:28 +0100

    xml via php 2 mysql

--- XML_transfer/export.php
+++ XML_transfer/export.php
@@ -1,46 +1,24 @@
 <?php
 
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `price` decimal(8,5) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+$xmlDoc = new DOMDocument();
+$xmlDoc->load("AstraST_001_DFI.xml");
+$mysql_hostname = "localhost"; // Example : localhost
+$mysql_user     = "root";
+$mysql_password = "";
+$mysql_database = "rosetta-app";
 
-$xmlData =<<< END
-<?xml version="1.0"?>
-<data>
-<item>
-<id>20</id>
-<name>mangoes</name>
-<price>11</price>
-</item>
-<item>
-<id>22</id>
-<name>strawberries</name>
-<price>5</price>
-</item>
-<item>
-<id>23</id>
-<name>grapes</name>
-<price>25</price>
-</item>
-</data>
-END;
-// read XML data string
-$xml = simplexml_load_string($xmlData) or die("ERROR: Cannot create SimpleXML object");
-// open MySQL connection
-$connection = mysqli_connect("localhost", "root", "", "rosetta-app") or die ("ERROR: Cannot connect");
-// process node data
-// create and execute INSERT queries
-foreach ($xml->item as $item) {
-    $id = $item->id;
-    $name = mysqli_real_escape_string($connection, $item->name);
-    $price = $item->price;
-    $sql = "INSERT INTO products (id, name, price) VALUES ('$id', '$name', '$price')";
-    mysqli_query($connection, $sql) or die ("ERROR: " .mysqli_error($connection) . " (query was $sql)");
+$bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Oops some thing went wrong");
+mysqli_select_db($mysqli_database, $bd) or die("Oops some thing went wrong");
+
+$xmlObject = $xmlDoc->getElementsByTagName('item');
+$itemCount = $xmlObject->length;
+
+for ($i=0; $i < $itemCount; $i++){
+    $de = $xmlObject->item($i)->getElementsByTagName('de')->item(0)->childNodes->item(0)->nodeValue;
+    $fr  = $xmlObject->item($i)->getElementsByTagName('fr')->item(0)->childNodes->item(0)->nodeValue;
+    $sql   = "INSERT INTO `my_table_name` (title, url) VALUES ('$de', '$fr')";
+    mysqli_query($sql);
+    print "Finished Item $de n<br/>";
 }
-// close connection
-mysqli_close($connection);
+
 ?>
