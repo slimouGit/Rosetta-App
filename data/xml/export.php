3280c4a7a9ce636f86680dfc29d9556d5443411a
Salim Oussayfi <box@slimou.net>
2017-03-11 11:45:50 +0100

    xml 2 sql  works in utf8  :)

--- data/xml/export.php
+++ data/xml/export.php
@@ -4,7 +4,7 @@
 header('Content-Type: text/html; charset=utf-8');
 
 $xmlDoc = new DOMDocument();
-$xmlDoc->load("MOKKA_dfi_008.xml");
+$xmlDoc->load("MOKKA_dfi_006.xml");
 $servername = "localhost";
 $username     = "root";
 $password = "";
