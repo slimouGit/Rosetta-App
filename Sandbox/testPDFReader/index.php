2ec76fe49e248316a3f1b85a15f4de4d4c1c1836
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-03-20 12:45:51 +0100

    testing PDF reader

---
+++ Sandbox/testPDFReader/index.php
@@ -0,0 +1,16 @@
+<?php
+$carline = "AstraST_df";
+$document = $carline . ".pdf";
+echo $document;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<object data="AstraST_df.pdf" type="application/pdf" width="100%" height="100%"></object>
+</body>
+</html>
