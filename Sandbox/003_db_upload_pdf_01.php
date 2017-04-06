c79c10ce6dc06b86890c76d601d4285663dea9de
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-04-06 16:22:06 +0200

    change name of new sides

---
+++ Sandbox/003_db_upload_pdf_01.php
@@ -0,0 +1,36 @@
+<?php
+//include header
+include "elements/header.php";
+?>
+
+<?php
+//include db connection
+include "include/db_connect.php";
+?>
+
+    <h2>Preislisten-PDFs hochladen</h2>
+    <p>Schreibweise muss eingehalten werden, d.h. evtl. Erklär-Funktion integrieren</p>
+
+<form enctype="multipart/form-data" action="003_db_upload_pdf_02.php" method="post">
+<div class="row">
+    <div class="form-group">
+        <label class="col-sm-1 control-label">Datei</label>
+        <div class="col-sm-4">
+            <input type="file" class="form-control" name="upfile" /></input>
+        </div>
+    </div>
+</div>
+
+<div class="row button">
+    <div class="form-group">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1">
+            <input type="submit" class="btn btn-primary" value="Senden"></input>
+        </div>
+    </div>
+</div>
+
+</form>
+
+
+
---
+++ Sandbox/003_db_upload_pdf_02.php
@@ -0,0 +1,69 @@
+<?php
+//include header
+include "elements/header.php";
+?>
+
+<?php
+//include db connection
+include "include/db_connect.php";
+?>
+
+    <h2>Preislisten-PDFs hochladen</h2>
+    <p>Schreibweise muss eingehalten werden, d.h. evtl. Erklär-Funktion integrieren</p>
+
+<div class="row">
+    <div class="col-sm-4">
+    <?php
+
+    $path = getcwd();
+    $target_dir = $path."/pl/";
+
+
+
+    /* Kontrolldaten */
+    echo "<p>Zur Kontrolle:<br />";
+    echo "Originaldateiname: "
+        . $_FILES["upfile"]["name"] . "<br />";
+    $fileName = $_FILES["upfile"]["name"];
+
+    $new_path = $target_dir . $fileName;
+
+
+    echo "Dateigroesse: "
+        . $_FILES["upfile"]["size"] . "<br />";
+    echo "Dateityp: "
+        . $_FILES["upfile"]["type"] . "<br />";
+
+    /* Dateiendung extrahieren */
+    $dname = explode(".",$_FILES["upfile"]["name"]);
+    $ext = $dname[count($dname)-1];
+
+    echo "Dateiendung: $ext<br />";
+
+    /* Temporaerer Dateiname auf dem Server */
+    echo "Temporaerer Dateiname: "
+        . $_FILES["upfile"]["tmp_name"] . "</p>";
+
+
+    if($_FILES["upfile"]["size"]>0)
+    {
+        copy($_FILES["upfile"]["tmp_name"],$new_path);
+        echo "<p>Datei wurde kopiert in {$target_dir}<br />";
+    }
+    else
+        echo "<p>Kopierfehler</p>";
+
+    function uploadFile($fileName, $new_path){
+        move_uploaded_file ($fileName, $new_path);
+
+        echo "new path: ".$new_path;
+    }
+
+    ?>
+    </div>
+</div>
+
+
+
+
+
