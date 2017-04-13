3f4631b3e8d432698b772c805bc15483dd553c4a
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-04-13 09:45:31 +0200

    connection for local and server implemented

--- Sandbox/loginSession/db_connect_PDO.php
+++ Sandbox/loginSession/db_connect_PDO.php
@@ -1,5 +1,10 @@
 <?php
+//local
 $pdo = new PDO('mysql:host=localhost;dbname=rosetta-app;charset=utf8', 'root', '');
+
+//online
+//$pdo = new PDO('mysql:host=rdbms.strato.de;dbname=DB2881460;charset=utf8', 'U2881460', 'Rosetta-App_1');
+
 
 //$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
