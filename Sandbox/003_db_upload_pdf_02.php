4e9a1cf7b1dad5823c2d8a70afa50452177b93be
Salim Oussayfi <box@slimou.net>
2017-04-21 17:22:12 +0200

    completed array of carlines for pdf uploading

--- Sandbox/003_db_upload_pdf_02.php
+++ Sandbox/003_db_upload_pdf_02.php
@@ -25,8 +25,82 @@ include "include/db_connect.php";
     echo "Originaldateiname: "
         . $_FILES["upfile"]["name"] . "<br />";
     $fileName = $_FILES["upfile"]["name"];
+    $fileName = strtolower($fileName);
 
-    $new_path = $target_dir . $fileName;
+    //Carline Array
+    $carlinesArray = array(
+        "ADAM",
+        "Ampera",
+        "Antara",
+        "AstraST",
+        "AstraNG",
+        "Cascada",
+        "ComboKastenwagen",
+        "ComboPassenger",
+        "Corsa",
+        "Corsa",
+        "GTC_OPC",
+        "InsigniaGS",
+        "InsigniaST",
+        "KARL",
+        "Meriva",
+        "MokkaX",
+        "MovanoCombiBus",
+        "MovanoFahrgestell",
+        "MovanoVan",
+        "Vivaro",
+        "Zafira",
+    );
+    /*
+    foreach ($carlinesArray AS $car){
+        if (preg_match('/$car/', $fileName)) {$carline = $car;}
+    }
+    */
+
+    //Carline testen
+    if (preg_match('/adam/', $fileName)) {$carline = "ADAM";}
+    if (preg_match('/ampera/', $fileName)) {$carline = "Ampera";}
+    if (preg_match('/antara/', $fileName)) {$carline = "Antara";}
+    if (preg_match('/astrast/', $fileName)) {$carline = "AstraST";}
+    if (preg_match('/astrang/', $fileName)) {$carline = "AstraNG";}
+    if (preg_match('/cascada/', $fileName)) {$carline = "Cascada";}
+    if (preg_match('/kastenwagen/', $fileName)) {$carline = "ComboKastenwagen";}
+    if (preg_match('/passenger/', $fileName)) {$carline = "ComboPassenger";}
+    if (preg_match('/corsa/', $fileName)) {$carline = "Corsa";}
+    if (preg_match('/crossland/', $fileName)) {$carline = "Crossland";}
+    if (preg_match('/gtc/', $fileName)) {$carline = "GTC_OPC";}
+    if (preg_match('/insigniags/', $fileName)) {$carline = "InsigniaGS";}
+    if (preg_match('/insigniast/', $fileName)) {$carline = "InsigniaST";}
+    if (preg_match('/karl/', $fileName)) {$carline = "KARL";}
+    if (preg_match('/meriva/', $fileName)) {$carline = "Meriva";}
+    if (preg_match('/mokka/', $fileName)) {$carline = "MokkaX";}
+    if (preg_match('/bus/', $fileName)) {$carline = "MovanoCombiBus";}
+    if (preg_match('/fahrgestell/', $fileName)) {$carline = "MovanoFahrgestell";}
+    if (preg_match('/van/', $fileName)) {$carline = "MovanoVan";}
+    if (preg_match('/vivaro/', $fileName)) {$carline = "Vivaro";}
+    if (preg_match('/zafira/', $fileName)) {$carline = "Zafira";}
+
+    //Sprache testen
+    if (preg_match('/_df/', $fileName)) {
+        $language = "df";
+    }else{
+        $language = "di";
+    }
+    //echo $language . "<br/>";
+    $language = implode('.', array($language, "pdf"));
+
+    //neuen Namen generieren
+    $newFileName = implode('_', array($carline, $language));
+
+    //var_dump($newFileName);
+
+    echo "Neuer Name: " . $newFileName. "<br/>";
+
+
+
+
+
+    $new_path = $target_dir . $newFileName;
 
 
     echo "Dateigroesse: "
