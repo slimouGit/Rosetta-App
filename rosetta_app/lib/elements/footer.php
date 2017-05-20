2952eab790d9eb58a52bccc91a379f93faf62ca0
Salim Oussayfi <box@slimou.net>
2017-05-20 05:41:59 +0200

    set imports in header and footer for local and online imports/ test css

--- rosetta_app/lib/elements/footer.php
+++ rosetta_app/lib/elements/footer.php
@@ -1,4 +1,14 @@
     </body>
+    <!-- IMPORTE -->
+
+    <!-- LOCAL -->
     <!-- script local -->
+    <!--
+    -->
     <script src="http://localhost/Rosetta-App/rosetta_app/lib/js/script_postLoaded.js"></script>
+
+    <!-- ONLINE -->
+    <!-- script online -->
+    <!--
+    <script src="http://rosetta-app.de/rosetta_app/lib/js/script_postLoaded.js"></script>
 </html>
--- rosetta_app/lib/elements/header.php
+++ rosetta_app/lib/elements/header.php
@@ -8,9 +8,30 @@
         <meta name="description" content="Praxis-Projekt Beuth-Hochschule/Medieninformatik B.Sc.">
         <title>Rosetta-App</title>
 
+        <!-- LOCAL -->
         <!-- Bootstrap local -->
+        <!--
+        -->
         <link href="http://localhost/Rosetta-App/rosetta_app/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- script local -->
+        <!--
+        -->
         <script src="http://localhost/Rosetta-App/rosetta_app/lib/js/script_preLoaded.js"></script>
+        <!-- css local -->
+        <!--
+        -->
+        <link rel="stylesheet" href="http://localhost/Rosetta-App/rosetta_app/lib/css/layout.css">
+
+        <!-- ONLINE -->
+        <!-- Bootstrap online -->
+        <!--
+        <link href="http://rosetta-app.de/rosetta_app/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- script online -->
+        <!--
+        <script src="http://rosetta-app.de/rosetta_app/lib/js/script_preLoaded.js"></script>
+        <!-- css online -->
+        <!--
+       <link rel="stylesheet" href="http://rosetta-app.de/rosetta_app/lib/css/layout.css">
+
     </head>
     <body>
