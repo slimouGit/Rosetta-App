d9370dafd6e135eccaca2546bb12504f7797b447
Salim Oussayfi <box@slimou.net>
2017-05-21 11:55:43 +0200

    new folder for user handling

---
+++ rosetta_app/data_user/register_user.php
@@ -0,0 +1,24 @@
+<?php
+//include header
+include "../lib/elements/header.php";
+?>
+
+<?php
+//include connection database
+include "../mvc/controller/db_connect.php";
+?>
+
+    <div class="container">
+        <div class='row'>
+            <h1>Rosetta-Data complete</h1>
+            <?php
+            //include table_items
+            include "../mvc/view/table_user.php";
+            ?>
+        </div>
+    </div>
+
+<?php
+//include header
+include "../lib/elements/footer.php";
+?>
