b103f2fa065286af3664b1a99ea07b9b8279a93a
Salim Oussayfi <box@slimou.net>
2017-05-21 17:20:13 +0200

    view "search_item.php"

---
+++ rosetta_app/data_center/data_handling/search_item.php
@@ -0,0 +1,22 @@
+<?php
+//include header
+include "../../lib/elements/header.php";
+?>
+
+<?php
+//include connection to database
+include "../../mc/controller/db_connect.php";
+?>
+
+    <div class="container">
+        <div class='row'>
+            <h1>Rosetta-Data search data</h1>
+
+        </div>
+    </div>
+
+
+<?php
+//include header
+include "../../lib/elements/footer.php";
+?>
