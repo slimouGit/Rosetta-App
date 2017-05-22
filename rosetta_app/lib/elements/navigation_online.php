fd6d64a337ee98f328721968aed47af8d40028e5
Salim Oussayfi <box@slimou.net>
2017-05-22 20:49:58 +0200

    online navigation

---
+++ rosetta_app/lib/elements/navigation_online.php
@@ -0,0 +1,38 @@
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-exmple-navbar-collapse-1" >
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://rosetta-app.de/">Rosetta-App</a>
+        </div>
+        <div class="collapse navbar-collapse navbar-left" id="bs-exmple-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="http://rosetta-app.de/rosetta_app/data_center/data_handling/search_item.php">Suchen</a></li>
+                <li><a href="http://rosetta-app.de/rosetta_app/data_center/data_handling/create_item.php"">Erstellen</a></li>
+                <li><a href="http://rosetta-app.de/rosetta_app/data_center/data_upload/upload_pdf.php">PDF hochladen</a></li>
+                <li><a href="http://rosetta-app.de/rosetta_app/data_center/data_upload/upload_xml.php">XML hochladen</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administration
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://rosetta-app.de/rosetta_app/data_user/register_user.php">Nutzer registrieren</a></li>
+                        <li><a href="http://rosetta-app.de/rosetta_app/data_user/edit_user.php">Benutzer verwalten</a></li>
+                        <li><a href="http://rosetta-app.de/rosetta_app/data_center/data_handling/data_complete.php">Daten gesamt</a></li>
+                        <li><a href="http://rosetta-app.de/rosetta_app/data_center/data_deleted/deleted_item.php">Daten gelöscht</a></li>
+                    </ul>
+                </li>
+                <li><a href="logout.php">logout</a></li>
+                <li><a href="neuesPasswort.php">Passwort ändern</a></li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
