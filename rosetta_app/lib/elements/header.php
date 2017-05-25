82e1bf2a10a4bcd96762a2eb1a6f6bedef2857c6
Salim Oussayfi <box@slimou.net>
2017-05-25 09:20:06 +0200

    header

--- rosetta_app/lib/elements/header.php
+++ rosetta_app/lib/elements/header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Salim Oussayfi">
+        <meta name="description" content="Praxis-Projekt Beuth-Hochschule/Medieninformatik B.Sc.">
+
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Rosetta-App</title>
         <meta charset="utf-8"/>
@@ -24,22 +31,22 @@
 
 
         <!-- LOCAL -->
-        <!-- Bootstrap local --
+        <!-- Bootstrap local -->
          <link href="http://localhost/Rosetta-App/rosetta_app/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- script local -->
         <script src="http://localhost/Rosetta-App/rosetta_app/lib/js/script_preLoaded.js"></script>
-        <!-- css local --
+        <!-- css local -->
         <link rel="stylesheet" href="http://localhost/Rosetta-App/rosetta_app/lib/css/layout.css">
         -->
 
         <!-- ONLINE -->
-        <!-- Bootstrap online -->
+        <!-- Bootstrap online --
         <link href="http://rosetta-app.de/rosetta_app/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- script online
         <script src="http://rosetta-app.de/rosetta_app/lib/js/script_preLoaded.js"></script>
-        <!-- css online -->
+        <!-- css online --
         <link rel="stylesheet" href="http://rosetta-app.de/rosetta_app/lib/css/layout.css">
-
+-->
 
 
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
