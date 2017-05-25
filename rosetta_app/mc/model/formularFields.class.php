7460aaf65649afb9dbea6eec0b3a6de5c6fc4c4d
Salim Oussayfi <box@slimou.net>
2017-05-25 09:19:52 +0200

    formularFields.class

--- rosetta_app/mc/model/formularFields.class.php
+++ rosetta_app/mc/model/formularFields.class.php
@@ -8,19 +8,19 @@
  */
 class formular
 {
-    public function inputField($label,$name){
-        echo "<div class=\"row\">
+    public function inputField($label,$name, $value, $placeholder, $id){
+        echo "<div class=\"row formMarginBottom\">
                     <div class=\"form-group\">
                         <label class=\"col-sm-2 control-label\">".$label."</label>
-                        <div class=\"col-sm-6\">
-                            <input type=\"text\" class=\"form-control\" size=\"40\" maxlength=\"250\" name=\"$name\">
+                        <div class=\"col-sm-8\">
+                            <input type=\"text\" class=\"form-control\" size=\"40\" maxlength=\"250\" name=\"$name\" value=\"$value\" placeholder=\"$placeholder\" id=\"$id\">
                         </div>
                     </div>
                 </div>";
     }
 
     public function submitButton($text){
-        echo "<div class=\"row\">
+        echo "<div class=\"row formMarginBottom\">
                         <div class=\"col-sm-2\"></div>
                         <div class=\"col-sm-1\">
                             <input type=\"submit\" class=\"btn btn-primary\" value=\"$text\">
@@ -30,7 +30,7 @@ class formular
     }
 
     public function selectStart($label){
-        echo "<div class=\"row\">
+        echo "<div class=\"row formMarginBottom\">
         <div class=\"form-group\">
             <label class=\"col-sm-2 control-label\">$label</label>
                 <div class=\"col-sm-6\">
