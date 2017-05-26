611c47cfda3a6b62c70c958382e4cedae4b42931
Salim Oussayfi <box@slimou.net>
2017-05-26 07:05:50 +0200

    date_delete will be set

--- rosetta_app/delete_item.php
+++ rosetta_app/delete_item.php
@@ -63,12 +63,20 @@ if(empty($_GET["data_id"])){
             }
 
              if(isset($_GET['delete_item'])) {
-                echo "<br/><br/>{$tempId} kommt an";
 
-                 $res = $pdo->prepare("UPDATE rosetta_data SET state = :state_neu, user_delete = :user_delete WHERE data_id = $tempId");
-                 $res->execute(array('state_neu' => 'deleted', 'user_delete' => $username));
+                $currentDate = date('d.m.Y H:i');
 
-                 echo "<br/><br/>{$tempId} wurde gelöscht";
+                $res = $pdo->prepare("UPDATE rosetta_data SET state = :state_neu, user_delete = :user_delete, date_delete = :date_delete WHERE data_id = $tempId");
+                $res->execute(array('state_neu' => 'deleted', 'user_delete' => $username, 'date_delete' => $currentDate));
+
+                //------------------------------------------------------------------------------------------
+
+                //Meldung wird ausgegeben
+                require_once "mc/model/responseObject.class.php";
+                $response = new responseText();
+                $response->success("Der Eintrag mit der ID {$tempId} wurde erfolgreich gelöscht");
+
+                //------------------------------------------------------------------------------------------
             }
 
             ?>
