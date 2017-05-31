13ed90b30296b909366e1df49f0c52c282be4f74
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-05-31 15:24:08 +0200

    outsourcing all js-functions

--- rosetta_app/lib/js/script_postLoaded.js
+++ rosetta_app/lib/js/script_postLoaded.js
@@ -4,7 +4,7 @@
 console.log("script_postLoaded");
 console.log("Test");
 
-    //Funktion
+    //Funktion fuer die dropdown-select-box mit Mehrfachauswahl-Checkbox
     $(document).ready(function() {
         $('#example-getting-started').multiselect();
     });
@@ -14,4 +14,30 @@ console.log("Test");
     function auto_grow(element) {
         element.style.height = "5px";
         element.style.height = (element.scrollHeight)+"px";
-    }
+    };
+
+    //Funktion zum Speichern in die Zwischenablage
+    //ueber temporaer hinzugefuegtes input-Feld
+    //gleichzeitig werden alle Leerzeichen geloescht,
+    //die dem Wert anhaengen
+    function copyToClipboard(element) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+
+        //kopierten Text in copiedValue speichern
+        var copiedValue = $(element).text();
+        //Leerzeichen ind copiedValue entfernen
+        while (copiedValue.indexOf('  ') > 0) {
+            copiedValue = copiedValue.replace('  ', '');
+            if(copiedValue.slice(-1)==' '){
+                //var deleteSpace = copiedValue.length-1;
+                copiedValue = copiedValue.slice(0, copiedValue.length-1);
+            }
+        }
+
+        $temp.val(copiedValue).select();
+        document.execCommand("copy");
+
+        console.log(copiedValue.length-1);
+        $temp.remove();
+    };
