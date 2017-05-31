e9659a3d29afd93595f09954be92e4d9beacbb77
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-05-31 11:07:14 +0200

    set autocomplete script as a controlling part

---
+++ rosetta_app/mc/controller/autocomplete.php
@@ -0,0 +1,24 @@
+<?php
+
+
+//include db connection
+include "db_connect.php";
+
+//$searchTem mit Variable "term" aus jquery-ui.js initialisieren
+$searchTerm = $_GET['term'];
+
+//
+$searchCategory = 'item_de, item_fr, item_it, category, info';
+
+
+$query = $pdo->query("SELECT * FROM rosetta_data WHERE CONCAT_WS('',$searchCategory) LIKE '%".$searchTerm."%' ORDER BY item_de ASC");
+while ($row = $query->fetch()) {
+    $data[] = $row['item_de'];
+    $data[] = $row['item_fr'];
+    $data[] = $row['item_it'];
+}
+
+//JSON-Object zurueckgeben
+echo json_encode($data);
+
+?>
