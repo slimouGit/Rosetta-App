c0118e6108c97dec0c36bae0b33f170d45c788c7
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-05-31 15:17:23 +0200

    outsourcing all js-functions

--- rosetta_app/lib/js/script_postLoaded.js
+++ rosetta_app/lib/js/script_postLoaded.js
@@ -2,10 +2,16 @@
  * Created by salim on 19.05.2017.
  */
 console.log("script_postLoaded");
+console.log("Test");
 
-
+    //Funktion
     $(document).ready(function() {
         $('#example-getting-started').multiselect();
     });
 
 
+    //Funktion zur automatischen Groessenanpassung der TExtarea
+    function auto_grow(element) {
+        element.style.height = "5px";
+        element.style.height = (element.scrollHeight)+"px";
+    }
--- rosetta_app/lib/js/script_preLoaded.js
+++ rosetta_app/lib/js/script_preLoaded.js
@@ -2,3 +2,11 @@
  * Created by salim on 19.05.2017.
  */
 console.log("script_preLoaded");
+
+//ergaenzende Funktion fuer die Autovervollstaendigung bei der Suche
+//uebergibt die ID der Sucheingabe an das Script in autocomplete.php
+$(function() {
+    $( "#skills" ).autocomplete({
+        source: 'mc/controller/autocomplete.php'
+    });
+});
