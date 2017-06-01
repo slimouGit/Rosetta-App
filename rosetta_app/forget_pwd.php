80d258447e3ddc15f56b717fcb96c2e87bb293fd
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-06-01 11:53:50 +0200

    extended password management works now :)

--- rosetta_app/forget_pwd.php
+++ rosetta_app/forget_pwd.php
@@ -14,100 +14,121 @@ include "mc/controller/db_connect.php";
 include 'lib/elements/navigationStart.php';
 ?>
 
+<!------------------------------------------>
+
 <div class="container-fluid content">
 
     <div class="col-lg-12">
         <div class='row'>
             <h1>Passwort vergessen</h1>
+            <p>Bitte die Email-Adresse angeben, an die das neue Passwort gesendet werden soll.</p>
         </div>
     </div>
 
-<?php
-function random_string() {
-    if(function_exists('random_bytes')) {
-        $bytes = random_bytes(16);
-        $str = bin2hex($bytes);
-    } else if(function_exists('openssl_random_pseudo_bytes')) {
-        $bytes = openssl_random_pseudo_bytes(16);
-        $str = bin2hex($bytes);
-    } else if(function_exists('mcrypt_create_iv')) {
-        //$bytes = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
-        $bytes = bin2hex(openssl_random_pseudo_bytes(16));
-        $str = bin2hex($bytes);
-    } else {
-        //Bitte euer_geheim_string durch einen zufälligen String mit >12 Zeichen austauschen
-        $str = md5(uniqid('rosetta-app', true));
-    }
-    return $str;
-}
+    <!------------------------------------------>
 
+    <?php
 
-$showForm = true;
-
-if(isset($_GET['send']) ) {
-    if(!isset($_POST['email']) || empty($_POST['email'])) {
-        $error = "<b>Bitte eine E-Mail-Adresse eintragen</b>";
-    } else {
-        $statement = $pdo->prepare("SELECT * FROM rosetta_users WHERE email = :email");
-        $result = $statement->execute(array('email' => $_POST['email']));
-        $user = $statement->fetch();
-
-        if($user === false) {
-            $error = "<b>Kein Benutzer gefunden</b>";
+    function random_string() {
+        if(function_exists('random_bytes')) {
+            $bytes = random_bytes(16);
+            $str = bin2hex($bytes);
+        } else if(function_exists('openssl_random_pseudo_bytes')) {
+            $bytes = openssl_random_pseudo_bytes(16);
+            $str = bin2hex($bytes);
+        } else if(function_exists('mcrypt_create_iv')) {
+            //$bytes = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+            $bytes = bin2hex(openssl_random_pseudo_bytes(16));
+            $str = bin2hex($bytes);
         } else {
-            //Überprüfe, ob der User schon einen Passwortcode hat oder ob dieser abgelaufen ist
-            $passwordcode = random_string();
+            //Bitte euer_geheim_string durch einen zufälligen String mit >12 Zeichen austauschen
+            $str = md5(uniqid('rosetta-app', true));
+        }
+        return $str;
+    }
 
-            $currentDate = date('Y-m-d H:i:s');
+    //-------------------------------------------------
 
-            $res = $pdo->prepare("UPDATE rosetta_users SET password_code = :password_code, password_date = :password_date WHERE user_id = :user_id");
-            $result = $res->execute(array('password_code' => sha1($passwordcode), 'password_date' => $currentDate, 'user_id' => $user['user_id']));
+    $showForm = true;
 
-            $empfaenger = $user['email'];
-            $betreff = "Neues Passwort für deinen Account auf www.rosetta-app.de";
-            $from = "From: Admin <admin@rosetta-app.de>";
-            $url_passwordcode = 'http://rosetta-app.de/rosetta_app/forget_pwd_reset.php?user_id='.$user['user_id'].'&code='.$passwordcode;
+    //-------------------------------------------------
 
-            $text = 'Hallo '.$user['forename'].',
-für deinen Account auf www.rosetta-app.de wurde nach einem neuen Passwort gefragt. Um ein neues Passwort zu vergeben, rufe innerhalb der nächsten 24 Stunden die folgende Website auf:
-'. $url_passwordcode
-.'
- 
-Sollte dir dein Passwort wieder eingefallen sein oder hast du dies nicht angefordert, so bitte ignoriere diese E-Mail.';
-            mail($empfaenger, $betreff, $text, $from);
+    if(isset($_GET['send']) ) {
+        if(!isset($_POST['email']) || empty($_POST['email'])) {
+            $error = "Bitte eine E-Mail-Adresse eintragen";
+        } else {
+            $statement = $pdo->prepare("SELECT * FROM rosetta_users WHERE email = :email");
+            $result = $statement->execute(array('email' => $_POST['email']));
+            $user = $statement->fetch();
 
-            //echo $url_passwortcode;
+            if($user === false) {
+                $error = "Kein Benutzer gefunden";
+            } else {
+                //Überpruefen, ob der User schon einen Passwortcode hat oder ob dieser abgelaufen ist
+                $passwordcode = random_string();
 
-            //echo "<br/><a href='".$url_passworcode."'>Link</a><br/>";
-            $test = 'forget_pwd_reset.php?user_id='.$user['user_id'].'&code='.$passwordcode;
-            echo "<br/><a href='".$test."'>Passwort zuruecksetzen</a><br/>";
+                $currentDate = date('Y-m-d H:i:s');
 
+                $res = $pdo->prepare("UPDATE rosetta_users SET password_code = :password_code, password_date = :password_date WHERE user_id = :user_id");
+                $result = $res->execute(array('password_code' => $passwordcode, 'password_date' => $currentDate, 'user_id' => $user['user_id']));
 
-            echo "Ein Link um dein Passwort zurückzusetzen wurde an deine E-Mail-Adresse gesendet.";
-            $showForm = false;
+                $empfaenger = $user['email'];
+                $betreff = "Neues Passwort für deinen Account auf www.rosetta-app.de";
+                $from = "From: admin@rosetta-app.de";
+                $url_passwordcode = 'http://rosetta-app.de/rosetta_app/forget_pwd_reset.php?user_id='.$user['user_id'].'&code='.$passwordcode;
+
+                $text = 'Hallo '.$user['forename'].',
+                    für deinen Account auf www.rosetta-app.de wurde nach einem neuen Passwort gefragt. 
+                    Um ein neues Passwort zu vergeben, rufe die folgende Website über den folgenden Link auf:                
+                    '. $url_passwordcode .'
+                    Sollte dir dein Passwort wieder eingefallen sein oder hast du dies nicht angefordert, so bitte ignoriere diese E-Mail.';
+
+                mail($empfaenger, $betreff, $text, $from);
+
+                echo "Es wurde ein Link an Deine Email gesendet.";
+                $showForm = false;
+            }
         }
     }
-}
 
-if($showForm):
-    ?>
+    //------------------------------------------------------
 
-    Gib hier deine E-Mail-Adresse ein, um ein neues Passwort anzufordern.<br><br>
+    if($showForm){
 
-    <?php
+    //Fehlermeldung
     if(isset($error) && !empty($error)) {
-        echo $error;
+        ?>
+        <div class="col-lg-12">
+            <div class='row'>
+                <div class="formWrapper col-lg-3 response responseFalse">
+                    <?php
+                    echo $error;
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
     }
     ?>
 
-    <form action="?send=1" method="post">
-        E-Mail:<br>
-        <!--<input type="email" name="email" value="<?php echo isset($_POST['email']) ? htmlentities($_POST['email']) : ''; ?>"><br>-->
-        <input type="email" name="email" value="<?php echo isset($_POST['password']) ? $_POST['password'] : ''; ?>"><br>
+    <!------------------------------------------>
 
-        <input type="submit" value="Neues Passwort">
-    </form>
+    <div class="formWrapper col-lg-3">
+        <div class="formField">
+            <form action="?send=1" method="post">
+                <input type="email" class="form-control" placeholder="Email" name="email" value="<?php echo isset($_POST['password']) ? $_POST['password'] : ''; ?>"><br>
+                <input type="submit" class="btn btn-primary" value="Passwort anfordern">
+            </form>
+        </div>
+    </div>
+
+    <!------------------------------------------>
 
     <?php
-endif; //Endif von if($showForm)
-?>
+    }; //Endif von if($showForm)
+    ?>
+
+    <?php
+    //include header
+    include "lib/elements/footer.php";
+    ?>
--- rosetta_app/forget_pwd_reset.php
+++ rosetta_app/forget_pwd_reset.php
@@ -14,7 +14,9 @@ include "mc/controller/db_connect.php";
 include 'lib/elements/navigationStart.php';
 ?>
 
-<div class="container-fluid content">
+    <!-------------------------------------------------------------->
+
+    <div class="container-fluid content">
 
 
     <div class="container">
@@ -22,37 +24,82 @@ include 'lib/elements/navigationStart.php';
         <!-------------------------------------------------------------->
 
         <div class='row'>
-            <h1>Rosetta-App change own password</h1>
+            <h1>Rosetta-App change password</h1>
         </div>
 
         <!------------------------------------------------------------->
 
-        <div class='row'>
-            <div class="formWrapper col-lg-8">
-                <div class="formField">
+        <?php
 
-                    <form action="?change_pwd=1" method="post">
+        //--------------------------------------------------------------------
+        //es wird unterschieden, ob der Nutzer ueber die Navigation oder aus "table_user.class" kommt,
+        //also, ob er sein eigenes Passwort oder als Administrator das eines Nutzers aendern will
 
-                        <?php
-                        require_once "mc/model/formularFields.class.php";
-                        $form = new formular();
-                        $form->passwordField("Passwort", "password", "", "", "", 2, 8);
-                        $form->passwordField("Passwort wiederholen", "password2", "", "", "", 2, 8);
-                        $form->submitButton("2", "Registrieren");
-                        ?>
+        //$_GET["user_id"] ist leer, wenn ueber Navigation "Passwort ändern" auf Seite gewechselt wird
+        if(empty($_GET["user_id"])){
+            //$_POST["user_id"] ist leer, wenn nicht ueber hidden-field im Formular uebergeben wurde
+            //dann wird $_POST["user_id"] mit der ID des aktuellen Nutzers initialisiert,
+            //er aendert also sein eigenes Passwort
+            if(empty($_POST['user_id'])){
+                $_POST['user_id'] = $userid;
+            }
+            $_GET["user_id"] = $_POST['user_id'];
+        };
 
-                    </form>
+        //$tempId wird mit der uebergebenen ID aus table_user.class initialisiert
+        $tempId = $_GET["user_id"];
+
+        //der uebergebene Code wird in $code abgelegt
+        $code = $_GET['code'];
+
+        //--------------------------------------------------------------------
+
+        $res = $pdo->query("SELECT * FROM rosetta_users WHERE user_id LIKE $tempId");
+
+        //
+        foreach ($res AS $row):
+
+            //Pruefen, ob der generierte Code mit dem in der Datenbank abgelegten Code uebereinstimmt
+            if($code===$row["password_code"]){
+            ?>
+
+            <!-------------------------------------------------------------->
+
+            <div class='row'>
+                <div class="formWrapper col-lg-8">
+                    <div class="formField">
+
+                        <form action="?change_pwd=1" method="post">
+
+                            <?php
+                            require_once "mc/model/formularFields.class.php";
+                            $form = new formular();
+                            $form->hiddenField("user_id", "" . $row["user_id"] . "");
+                            $form->passwordField("Passwort", "password", "", "", "", 2, 8);
+                            $form->passwordField("Passwort wiederholen", "password2", "", "", "", 2, 8);
+                            $form->submitButton("2", "Registrieren");
+                            ?>
+
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <!-------------------------------------------------------------->
+
+            <?php
+                }//ENDE if($code===$row["password_code"])
+            endforeach;
+        ?>
 
         <!------------------------------------------------------------->
 
         <div class="container">
             <div class='row'>
                 <?php
-                $user = $_GET['user_id'];
-                echo $user;
+
+                //----------------------------------------------------------------
+
                 if(isset($_GET['change_pwd'])) {
                     $error = false;
                     $password = $_POST['password'];
@@ -73,7 +120,7 @@ include 'lib/elements/navigationStart.php';
                         $passwordhash = password_hash($password, PASSWORD_DEFAULT);
 
                         $res = $pdo->prepare("UPDATE rosetta_users SET password = :passwordhash WHERE user_id = :user_id");
-                        $result = $res->execute(array('passwordhash' => $passwordhash, 'user_id'=> $userid ));
+                        $result = $res->execute(array('passwordhash' => $passwordhash, 'user_id'=> $tempId ));
 
                         if($result) {
 
@@ -81,10 +128,15 @@ include 'lib/elements/navigationStart.php';
                             require_once "mc/model/responseObject.class.php";
                             $response = new responseObject();
                             $response->response("Das Passwort wurde erfolgreich geändert.", "4", "");
+
+                            echo "<a href=\"../index.php\">zum login</a>";
                         }
                     }
                 }
                 ?>
+
+                <!-------------------------------------------------------------->
+
             </div>
         </div>
 
@@ -92,78 +144,9 @@ include 'lib/elements/navigationStart.php';
 
     </div>
 
-
-
+    <!-------------------------------------------------------------->
 
 <?php
-/*
-if(isset($_GET['reset_pwd'])) {
-
-
-$user = $_GET['user_id'];
-
-$code = $_GET['code'];
-
-echo "USER: " .$user;
-echo "Code: " .sha1($code);
-echo "<br/>";
-echo "UserCode: " . $user['password_code'];
-echo "<br/>";
-echo "User:" . $user;
-//Abfrage des Nutzers
-$res = $pdo->prepare("SELECT * FROM rosetta_users WHERE user_id = :user_id");
-$result = $res->execute(array('user_id' => $user));
-$user = $res->fetch();
-
-//Überprüfe dass ein Nutzer gefunden wurde und dieser auch ein Passwortcode hat
-if($user === null || $user['password_code'] === null) {
-    die("Es wurde kein passender Benutzer gefunden");
-}
-
-if($user['password_date'] === null || strtotime($user['password_date']) < (time()-24*3600) ) {
-    die("Der Code ist nicht mehr gültig");
-}
-
-
-//Überprüfe den Passwortcode
-if(sha1($code) != $user['password_code']) {
-    die("Der übergebene Code war ungültig.");
-}
-}
-
-//Der Code war korrekt, der Nutzer darf ein neues Passwort eingeben
-
-if(isset($_GET['reset_pwd'])) {
-    $password = $_POST['password'];
-    $password2 = $_POST['password2'];
-    $user = $_GET['user_id'];
-
-    echo $user;
-
-    if($password != $password2) {
-        echo "Beide Passwörter müssen übereinstimmen";
-    } else { //Speichere neues Passwort und lösche den Code
-        $passwordhash = password_hash($password, PASSWORD_DEFAULT);
-
-        $res = $pdo->prepare("UPDATE rosetta_users SET password = :passwordhash WHERE user_id = :user_id");
-        $result = $res->execute(array('passwordhash' => $passwordhash, 'user_id'=> $user ));
-
-        if($result) {
-            die("Dein Passwort wurde erfolgreich geändert");
-        }
-    }
-}
+//include header
+include "lib/elements/footer.php";
 ?>
-
-    <h1>Neues Passwort vergeben</h1>
-    <form action=?reset_pwd=1" method="post">
-        Bitte gib ein neues Passwort ein:<br>
-        <input type="password" name="password"><br><br>
-
-        Passwort erneut eingeben:<br>
-        <input type="password" name="password2"><br><br>
-
-        <input type="submit" value="Passwort speichern">
-    </form>
-
-*/
