51d2e8be89be613ea6a25e55b6aaaa932bf36d7d
Salim Oussayfi <box@slimou.net>
2017-06-04 19:03:13 +0200

    after xml-file is copied and data were written to db, the file will be copied to another directory and the origin deleted

--- rosetta_app/mvc/model/upload_xml_model.php
+++ rosetta_app/mvc/model/upload_xml_model.php
@@ -1,19 +1,18 @@
 <?php
+
+
 $path = getcwd();
-echo "PFAD: ".$path;
-//$target_dir = "../../".__DIR__."/";
-
 $target_dir = $path."/";
-
-
 $fileName = $_FILES["xmlFile"]["name"];
 $new_path = $target_dir . $fileName;
-echo "Originaldateiname: "
-    . $fileName."<br />";
+
+echo "Originaldateiname: "  . $fileName."<br />";
+
 /* Temporaerer Dateiname auf dem Server */
-echo "Temporaerer Dateiname: "
-    . $_FILES["xmlFile"]["tmp_name"] . "</p>";
+echo "Temporaerer Dateiname: " . $_FILES["xmlFile"]["tmp_name"] . "</p>";
+
 copy($_FILES["xmlFile"]["tmp_name"],$new_path);
+
 echo "<p>Datei wurde kopiert in {$target_dir}<br />";
 echo "Der neue Pfad lautet: ".$new_path. "<br/>";
 
@@ -29,6 +28,9 @@ $token = (string)$token;
 $username = $username;
 $xmlObject = $xmlDoc->getElementsByTagName('item');
 $itemCount = $xmlObject->length;
+
+//-------------------------------------------------------------------------------------------------
+
 for ($i=0; $i < $itemCount; $i++){
     $item_de = $xmlObject->item($i)->getElementsByTagName('item_de')->item(0)->childNodes->item(0)->nodeValue;
     $item_fr  = $xmlObject->item($i)->getElementsByTagName('item_fr')->item(0)->childNodes->item(0)->nodeValue;
@@ -36,11 +38,21 @@ for ($i=0; $i < $itemCount; $i++){
     $category  = $xmlObject->item($i)->getElementsByTagName('category')->item(0)->childNodes->item(0)->nodeValue;
     $info  = $xmlObject->item($i)->getElementsByTagName('info')->item(0)->childNodes->item(0)->nodeValue;
     $carline  = $xmlObject->item($i)->getElementsByTagName('carline')->item(0)->childNodes->item(0)->nodeValue;
-    //global $res;
     //---------------------------------------------------------------------------------------
     //Daten werden eingefuegt
     $res = $pdo->prepare("INSERT INTO rosetta_data (token, item_de, item_fr, item_it, category, info, carline, user_create) VALUES (:token, :item_de, :item_fr, :item_it, :category, :info, :carline, :user_create)");
     $res->execute(array('token' => $token, 'item_de' => $item_de, 'item_fr' => $item_fr, 'item_it' => $item_it, 'category' => $category, 'info' => $info, 'carline' => $carline, 'user_create' => $username));
     print "Added Data $i: <br/> $item_de <br/> $item_fr<br/> $item_it<br/> $carline<br/> <br/> ";
 }
+
+//-----------------------------------------------------------------------------------------------
+
+//XML wird in anderes Verzeichnis kopiert und anschliessend aus dem urspuenglichen
+$path = getcwd();
+$source = $path."/".$fileName;
+$desk = $path."/uploaded_data/xml/".$fileName;
+
+copy($source,$desk);
+unlink($source);
+
 ?>
