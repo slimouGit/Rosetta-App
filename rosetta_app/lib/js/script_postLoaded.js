b01305be6fa648ddd1f537fd1a9daf569d07ca63
Salim Oussayfi <box@slimou.net>
2017-06-06 11:46:39 +0200

    move copyToClipboard() from script_postLoaded to script_preLoaded

--- rosetta_app/lib/js/script_postLoaded.js
+++ rosetta_app/lib/js/script_postLoaded.js
@@ -18,28 +18,3 @@ console.log("script_postLoaded");
 
 
 
-    //Funktion zum Speichern in die Zwischenablage
-    //ueber temporaer hinzugefuegtes input-Feld
-    //gleichzeitig werden alle Leerzeichen geloescht,
-    //die dem Wert anhaengen
-    function copyToClipboard(e) {
-        var $temp = $("<input>");
-        $("body").append($temp);
-
-        //kopierten Text in copiedValue speichern
-        var copiedValue = $(e).text();
-        //Leerzeichen ind copiedValue entfernen
-        while (copiedValue.indexOf('  ') > 0) {
-            copiedValue = copiedValue.replace('  ', '');
-            if(copiedValue.slice(-1)==' '){
-                //var deleteSpace = copiedValue.length-1;
-                copiedValue = copiedValue.slice(0, copiedValue.length-1);
-            }
-        }
-
-        $temp.val(copiedValue).select();
-        document.execCommand("copy");
-
-        console.log(copiedValue.length-1);
-        $temp.remove();
-    };
--- rosetta_app/lib/js/script_preLoaded.js
+++ rosetta_app/lib/js/script_preLoaded.js
@@ -3,6 +3,32 @@
  */
 console.log("script_preLoaded");
 
+//Funktion zum Speichern in die Zwischenablage
+//ueber temporaer hinzugefuegtes input-Feld
+//gleichzeitig werden alle Leerzeichen geloescht,
+//die dem Wert anhaengen
+function copyToClipboard(e) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+
+    //kopierten Text in copiedValue speichern
+    var copiedValue = $(e).text();
+    //Leerzeichen ind copiedValue entfernen
+    while (copiedValue.indexOf('  ') > 0) {
+        copiedValue = copiedValue.replace('  ', '');
+        if(copiedValue.slice(-1)==' '){
+            //var deleteSpace = copiedValue.length-1;
+            copiedValue = copiedValue.slice(0, copiedValue.length-1);
+        }
+    }
+
+    $temp.val(copiedValue).select();
+    document.execCommand("copy");
+
+    console.log(copiedValue.length-1);
+    $temp.remove();
+};
+
 //ergaenzende Funktion fuer die Autovervollstaendigung bei der Suche
 //uebergibt die ID der Sucheingabe an das Script in autocomplete.php
 $(function() {
