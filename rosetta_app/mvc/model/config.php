47f86876f4ec3af5628e76c907a02fda14d170ef
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-06-08 16:35:38 +0200

    config.php with preparation values for input fields

--- rosetta_app/mvc/model/config.php
+++ rosetta_app/mvc/model/config.php
@@ -6,27 +6,23 @@
  * Time: 11:54
  */
 
-//carlineArray initialisieren
-$carlineArray = array(
-    "ADAM",
-    "Ampera",
-    "Antara",
-    "AstraST",
-    "AstraNG",
-    "Cascada",
-    "ComboKastenwagen",
-    "ComboPassenger",
-    "Corsa",
-    "Corsa",
-    "GTC_OPC",
-    "InsigniaGS",
-    "InsigniaST",
-    "KARL",
-    "Meriva",
-    "MokkaX",
-    "MovanoCombiBus",
-    "MovanoFahrgestell",
-    "MovanoVan",
-    "Vivaro",
-    "Zafira",
-);
+if(empty($_POST['email'])){
+    $_POST['email']="";
+}
+
+if(empty($_POST['password'])){
+    $_POST['password']="";
+}
+
+if(empty($_POST['password2'])){
+    $_POST['password2']="";
+}
+
+if(empty($_POST['forename'])){
+    $_POST['forename']="";
+}
+
+if(empty($_POST['surname'])){
+    $_POST['surname']="";
+}
+
