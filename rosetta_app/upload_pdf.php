4c741ccc1c469b018d7dc09fbdf50bfafe08b06a
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-06-26 17:06:42 +0200

    alternative forms for xml and pdf upload

--- rosetta_app/upload_pdf.php
+++ rosetta_app/upload_pdf.php
@@ -25,20 +25,16 @@ include "mvc/model/db_connect_model.php";
             <div class="container">
                 <div class="panel panel-default">
                     <div class="panel-body">
-
-                        <h4>PDF-Datei wählen</h4>
-                        <form action="?upload_pdf=1" method="post" enctype="multipart/form-data" id="js-upload-form">
-                            <div class="input-group image-preview">
-                                <input placeholder="PDF wählen" type="button" class="form-control image-preview-filename" >
-                                <span class="input-group-btn">
-                            <button type="button" class="btn btn-default image-preview-clear" style="display:none;"> <span class="glyphicon glyphicon-remove"></span> Clear </button>
-                            <div class="btn btn-default image-preview-input"> <span class="glyphicon glyphicon-folder-open"></span> <span class="image-preview-input-title">Browse</span>
+                    <h4>PDF auswählen</h4>
+                        <form action="?upload_pdf=1" method="post" enctype="multipart/form-data">
+                        <div class="form-inline">
+                            <div class="form-group">
                                 <input type="file" class="form-control" name="upfile"/>
                             </div>
-                            <button type="submit" class="btn btn-labeled btn-primary"> <span class="btn-label"><i class="glyphicon glyphicon-upload"></i> </span>Hochladen</button>
-                            </span> </div>
-                    </div>
+                            <button type="submit" class="btn btn-sm btn-primary">Hochladen</button>
+                        </div>
                     </form>
+                    </div>
                 </div>
             </div>
 
