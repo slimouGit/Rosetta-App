a2342e5c5ed4724a57723bbf60494af97ac2c57d
Salim Oussayfi <salimoussayfi@Salim-Oussayfi.local>
2017-06-29 13:55:32 +0200

    search by column now imlemented

--- rosetta_app/search_item.php
+++ rosetta_app/search_item.php
@@ -16,11 +16,34 @@ include "lib/elements/header.php";
     <!------------------------------------------------------------->
 
     <div class='row'>
-        <div class="formWrapper col-lg-8">
+        <div class="formWrapper col-lg-8 ">
             <div class="formField">
                 <form action="?search_item=1" method = "post">
-                    <div class="col-sm-6 ui-widget">
+
+                    <!--Inputfeld zur Suche hier nicht uber Klasse implementiert-->
+                    <div class="row formMarginBottom">
+                    <div class="col-sm-10 ui-widget">
                         <input type="text" class="form-control" id="skills" name="search" value="<?php echo isset($_POST['search']) ? $_POST['search'] : ''; ?>" placeholder="Suchbegriff">
+                    </div>
+                    </div>
+
+
+                    <div class="row formMarginBottom">
+                        <div class="col-sm-2 control-label">Einschränken</div>
+                        <div class="col-sm-8">
+                            <?php
+                                //ueber die Checkboxen kann die Suche eingeschraenkt werden
+                                require_once "mvc/view/formularFields_view.class.php";
+                                $form = new formular();
+
+                                $form->checkboxFiltering("item_de", "Deutsch");
+                                $form->checkboxFiltering("item_fr", "Französisch");
+                                $form->checkboxFiltering("item_it", "Italienisch");
+                                $form->checkboxFiltering("category", "Rubrik");
+                                $form->checkboxFiltering("info", "Info");
+                                $form->checkboxFiltering("carline", "Carline");
+                            ?>
+                        </div>
                     </div>
 
                     <div class="row">
@@ -43,12 +66,15 @@ include "lib/elements/header.php";
         <div class='row'>
             <?php
 
+
+
             //Meldung wird ausgegeben
             require_once "mvc/view/responseObject_view.class.php";
             $response = new responseObject();
 
             if(isset($_GET['search_item'])) {
                 $error = false;
+
                 if (strlen($_POST['search']) == 0) {
 
                     $response->response("Bitte einen Suchbegriff eingeben","6","responseFalse");
@@ -59,11 +85,22 @@ include "lib/elements/header.php";
 
                     $tempItem = $_POST['search'];
 
+                    //Pruefung, ob checkboxen ausgewaehlt wurden
+                    if(!empty($_POST['searchCategory'])) {
+                        //das Array carline wird ueber implode in $car gespeichert
+                        $cat = implode(',  ', $_POST['searchCategory']);
+                    }
+                    //falls keine checkbox aktiv, wird in allen Spalten gesucht
+                    if(empty($_POST['searchCategory'])) {
+                       $cat = 'item_de, item_fr, item_it, category, info, carline';
+                    }
+
                     require "mvc/model/select_data_model.class.php";
-                    select_data::select_searchedData($tempItem);
+                    select_data::select_searchedData($cat, $tempItem);
 
                     //pruefen, ob Suche ein Resultat ergibt
                     $count = $res->rowCount();
+
                     if ($count == 0) {
 
                         $response->response("Kein Ergebnis für {$_POST['search']} verfügbar","6","responseFalse");
