bee26e428d8a1969f3ffb5b510d66a3d0d6201cc
MIMISOU\Salim <box@slimou.net>
2017-07-02 15:08:36 +0200

    sql script with two tables for the database "rosetta_db"

--- finalProject/Praxisprojekt_SS17_SalimOussayfi_797754/system/rosetta_app/mvc/model/db_connect_model.php
+++ finalProject/Praxisprojekt_SS17_SalimOussayfi_797754/system/rosetta_app/mvc/model/db_connect_model.php
@@ -8,10 +8,10 @@
  */
 
 //local
-//$pdo = new PDO('mysql:host=localhost;dbname=rosetta_db;charset=utf8', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=rosetta_db;charset=utf8', 'root', '');
 
 //online
-$pdo = new PDO('mysql:host=rdbms.strato.de;dbname=DB2988413;charset=utf8', 'U2988413', 'rosetta_db_3');
+//$pdo = new PDO('mysql:host=rdbms.strato.de;dbname=DB2988413;charset=utf8', 'U2988413', 'rosetta_db_3');
 
 
 
